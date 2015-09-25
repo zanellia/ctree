f8c68618332975793996c66b7ee265992db41725
Leonard Truong <leonardtruong@gmail.com>
2015-09-25 08:34:16 -0700

    Generate code using map for implicit vectorization

--- examples/hwacha.py
+++ examples/hwacha.py
@@ -1,10 +1,12 @@
 import numpy as np
 import ctypes as ct
+import ast
 
 from ctree.jit import LazySpecializedFunction, ConcreteSpecializedFunction
 from ctree.transformations import PyBasicConversions
 from ctree.transforms.declaration_filler import DeclarationFiller
 from ctree.c.nodes import CFile
+import ctree.c.nodes as C
 from ctree.nodes import Project
 
 
@@ -21,6 +23,24 @@ class HwachaFN(ConcreteSpecializedFunction):
         return self._c_function(*args)
 
 
+class MapTransformer(ast.NodeTransformer):
+    def __init__(self, loopvar, param_dict, retval_name):
+        self.loopvar = loopvar
+        self.param_dict = param_dict
+        self.retval_name = retval_name
+
+    def visit_SymbolRef(self, node):
+        if node.name in self.param_dict:
+            return C.ArrayRef(node, C.SymbolRef(self.loopvar))
+        return node
+
+    def visit_Return(self, node):
+        node.value = self.visit(node.value)
+        return C.Assign(C.ArrayRef(C.SymbolRef(self.retval_name),
+                                   C.SymbolRef(self.loopvar)), 
+                        node.value)
+
+
 class HwachaTranslator(LazySpecializedFunction):
     def args_to_subconfig(self, args):
         return tuple(get_nd_pointer(arg) for arg in args)
@@ -28,9 +48,25 @@ class HwachaTranslator(LazySpecializedFunction):
     def transform(self, py_ast, program_cfg):
         arg_cfg, tune_cfg = program_cfg
         tree = PyBasicConversions().visit(py_ast)
+        param_dict = {}
         # Annotate arguments
         for param, type in zip(tree.body[0].params, arg_cfg):
             param.type = type()
+            param_dict[param.name] = arg_cfg
+        tree.body[0].params.append(C.SymbolRef("retval", arg_cfg[0]()))
+
+        length = np.prod(arg_cfg[0]._shape_)
+        transformer = MapTransformer("i", param_dict, "retval")
+        body = map(transformer.visit, tree.body[0].defn)
+
+        tree.body[0].defn = [C.For(
+            C.Assign(C.SymbolRef("i", ct.c_int()), C.Constant(0)),
+            C.Lt(C.SymbolRef("i"), C.Constant(length)),
+            C.PostInc(C.SymbolRef("i")),
+            body=body,
+            pragma="ivdep"
+        )]
+
 
         tree = DeclarationFiller().visit(tree)
         return [CFile("generated", [tree])]
@@ -76,7 +112,19 @@ def gold(cold, hot, raw, flat):
         scaled = 0.0 if scaled < 0.0 else scaled
         flat[i] = 255 * scaled
 
-test = HwachaTranslator.from_function(gold, "Gold")
+def test_map(cold, hot, raw):
+    _max = hot
+    _min = cold
+    offset = raw - _min
+    scale = _max - _min
+    foffset = float(offset)
+    fscale = float(scale)
+    scaled = foffset / fscale
+    scaled = 1.0 if scaled > 1.0 else scaled
+    scaled = 0.0 if scaled < 0.0 else scaled
+    return 255 * scaled
+
+test = HwachaTranslator.from_function(test_map, "test")
 
 flat_gold = np.empty_like(raw)
 gold(cold, hot, raw, flat_gold)
