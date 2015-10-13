f3db59324b976278bbd6dc4d17a5af389086557d
chick <chick@qrhino.com>
2015-10-13 10:09:51 -0700

    artifact of trying to get test_examples to work

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,1 +1,16 @@
 __author__ = 'Chick Markley'
+
+__all__ = [
+    "ArrayDoubler",
+    "AstToDot",
+    "dgemm",
+    "Distrib",
+    "Fibonacci",
+    "hwacha",
+    "OclDoubler",
+    "OmpSpecializer",
+    "SimpleTranslator",
+    "stencil_grid/",
+    "TemplateDoubler",
+    "TuningSpecializer",
+]
