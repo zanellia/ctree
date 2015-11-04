fc2a8152b036d83bb50c15915539e97a7af4a2a0
chick <chick@qrhino.com>
2015-11-04 10:13:04 -0800

    playing with 3 nested loops

--- examples/dgemm2.py
+++ examples/dgemm2.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
+from ctree import get_ast, browser_show_ast
 from ctree.jit import ConcreteSpecializedFunction, LazySpecializedFunction
 import numpy as np
+from ctree.transformations import PyBasicConversions
 
 __author__ = 'Chick Markley chick@eecs.berkeley.edu U.C. Berkeley'
 
@@ -14,31 +16,53 @@ class ConcreteDgemm(ConcreteSpecializedFunction):
 
 
 class Dgemm(LazySpecializedFunction):
-    def multiply(self, a, b, c, alpha=1.0, beta=0.0):
+    def __init__(self):
+        tree = get_ast(Dgemm.multiply)
+        super(Dgemm, self).__init__(tree)
+
+    @staticmethod
+    def multiply(a, b, c=None, alpha=1.0, beta=0.0):
         n = a.shape[0]
         m = a.shape[1]
         p = b.shape[1]
 
         assert m == b.shape[0]
-        assert c.shape == (n, p)
+        if c is None:
+            c = np.zeros((a.shape[0], b.shape[1]))
+        else:
+            assert c.shape == (n, p)
 
         for i in range(n):
             for j in range(p):
                 c[(i,j)] *= beta
                 for k in range(m):
                     c[(i, j)] += alpha * a[(i, k)] * b[(k, j)]
+        return c
+
+    def transform(self, tree, program_config):
+        transforms = [
+            PyBasicConversions()
+        ]
+
+        for t in transforms:
+            tree = t.visit(tree)
+
+        browser_show_ast(tree)
+
 
 
 if __name__ == '__main__':
-    dgemm = Dgemm()
-
     a = np.array([[1, 2, 3], [4, 5, 6]])
     b = np.array([[1, 3, 5, 8], [2, 4, 6, 8], [3, 6, 9, 12]])
 
     print("a.shape {} b.shape {}".format(a.shape, b.shape))
 
-    c = np.zeros((a.shape[0], b.shape[1]))
+    c = Dgemm.multiply(a, b)
 
-    dgemm.multiply(a, b, c)
+    print("c\n{}".format(c))
+
+    dgemm = Dgemm()
+
+    c = dgemm(a, b)
 
     print("c\n{}".format(c))
